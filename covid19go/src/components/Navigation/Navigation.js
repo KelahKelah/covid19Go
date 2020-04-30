ed9caa2e73640969c79bfe649b65aacc43080782
KelahKelah <kelechiekebor@gmail.com>
2020-04-30 16:06:43 +0100

    add more components and styles

--- covid19go/src/components/Navigation/Navigation.js
+++ covid19go/src/components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link, BrowserRouter } from 'react-router-dom';
+import { Link, NavLink, BrowserRouter } from 'react-router-dom';
+import { FaVine, FaCaretDown } from 'react-icons/fa';
 import './Navigation.css';
 
 
@@ -7,10 +8,15 @@ const Navigation = () => {
     return(
         <>
             <BrowserRouter>
-                <div className='enwrap'>
-                    <p><Link to='/' className='item'>Home</Link></p>
-                    <p><Link to='/Donate' className='item'>Donate</Link></p>
-                    <p><Link to='/News' className='item'>News</Link></p>
+                <div className='nav_wrap'>
+                    <div className='logo_brand'>
+                        <p className='logo_label'><FaVine className='logo' />COVID19G0</p>
+                    </div>
+                    <div className='wrap'>
+                        <p><Link to='/' className='item'>Home</Link></p>
+                        <p><NavLink to='/Donate' className='item'>Donate</NavLink></p>
+                        <p><NavLink to='/News' className='item'>News</NavLink></p>
+                    </div>
                 </div>
             </BrowserRouter>
         </>
--- covid19go/src/routes.js
+++ covid19go/src/routes.js
@@ -4,20 +4,23 @@ import Navigation from './components/Navigation/Navigation';
 import Home from './pages/Home/Home';
 import Donate from './pages/Donate/Donate';
 import News from './pages/News/News';
+import Footer from './components/Footer/Footer';
 
 const Router = () => {
     return(
         <Fragment>
             <BrowserRouter>
-
+            {/* <div className=""> */}
             <Navigation />
-            <Switch>
-                <Route exact path='/' component={Home} Hom  />
-                <Route exact path='/Home' component={Home} />
-                <Route exact path='/Donate' component={Donate} />
-                <Route exact path='/News' component={News} />
-            </Switch>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/home' component={Home} />
+                    <Route exact path='/Donate' component={Donate} />
+                    <Route exact path='/News' component={News} />
+                </Switch>
+            {/* </div> */}
         </BrowserRouter>
+        <Footer />
     </Fragment>
     )
    
