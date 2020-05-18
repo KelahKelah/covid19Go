d922f2b7c98bc44dc3d3dbeb560021bb0945eddc
KelahKelah <kelechiekebor@gmail.com>
2020-05-18 04:00:56 +0100

    add cache/babel

---
+++ build/precache-manifest.3d6e0960117d442200a1bda194dbc8c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1bf4ea96fae890994b2cee3dd6eaf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fb1b50a14cf97b51563",
+    "url": "/static/css/main.f4a4dc52.chunk.css"
+  },
+  {
+    "revision": "ab406b7b62055640c3ad",
+    "url": "/static/js/2.4b3432f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4b3432f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb1b50a14cf97b51563",
+    "url": "/static/js/main.5d2fcf57.chunk.js"
+  },
+  {
+    "revision": "8198845d6b299cf132c1",
+    "url": "/static/js/runtime-main.d9d02f01.js"
+  }
+]);
