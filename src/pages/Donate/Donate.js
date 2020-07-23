27eece10145d22b39ce8080550647cd4e2d16205
KelahKelah <kelechiekebor@gmail.com>
2020-07-23 14:05:32 +0100

    modified donate page

--- src/pages/Donate/Donate.js
+++ src/pages/Donate/Donate.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import './Donate.css';
 import Modal from '../../components/Modal/Modal'
-import {FaQuoteLeft, FaQuoteRight, FaCaretRight} from 'react-icons/fa'
+import {FaQuoteLeft, FaQuoteRight } from 'react-icons/fa'
 
 const Donate = (props) => {
     useEffect(() => {
@@ -11,24 +11,22 @@ const Donate = (props) => {
     return(
         <>
             <div className="donate-wrap">
-            <Modal 
-        
-             />
+            
                 <div className='heading'>
                     <h2>Donate to Covid19GO!</h2>
                 </div>
                 <div className='wrap-two'>
                     <div className="donate-text">
                         <i><FaQuoteLeft /></i>
-                        <p className='text'>Covid19GO is an independent, non-profit organization passionate about helping covid19 victims all over the world. 
+                        <p>Covid19GO is an independent, non-profit organization passionate about helping covid19 victims all over the world. 
                                 This includes sponsoring victims for proper medical care, providing essentials such as drugs, food items, 
                                 facemask and so much more. 
                         </p>
-                        <p className='text'>We do believe that our contributions will go a long way in kicking covid19 out of our universe. We rely entirely 
+                        <p>We do believe that our contributions will go a long way in kicking covid19 out of our universe. We rely entirely 
                             on donations and receive no money from any governments or corporations. Our tiny online team ensures even the 
                             smallest contributions go a long way.
                         </p>
-                        <p className='text'>If you have any questions about how we intend to use funds, 
+                        <p>If you have any questions about how we intend to use funds, 
                             please feel free to email us here. Our payment process is fully encrypted to 
                             ensure all information is kept safe us. 
                             
@@ -38,12 +36,20 @@ const Donate = (props) => {
                     
                     <div className="payment">
                         <div>
-                            <button>Next Step<FaCaretRight /></button>
+                            <button className="btn btn-warning" data-toggle="" data-taget="#paymentModal">Donate</button>
                         </div>
                         
                     </div>             
                 </div>
                 
+            </div>
+            {/* Payment Modal  */}
+            <div data-target="id">
+                <div class="modal-head"></div>
+                <div class="modal-content">
+                    {/* <p>Yes</p> */}
+                </div>
+
             </div>
         </>
     )
