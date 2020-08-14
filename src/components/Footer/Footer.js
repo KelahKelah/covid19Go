de3c4b03357bef946f63697b9b2be9129feeb886
KelahKelah <kelechiekebor@gmail.com>
2020-08-14 13:34:03 +0100

    Fixed the mobile view

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,50 +1,68 @@
-import React from 'react';
-import { FaVine, FaTwitter, FaFacebook, FaInstagram, FaYoutube, FaWifi } from 'react-icons/fa';
-import './Footer.css';
-import '../Navigation/Navigation';
+import React from "react";
+import {
+  FaVine,
+  FaTwitter,
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaWifi,
+} from "react-icons/fa";
+import "./Footer.css";
+import "../Navigation/Navigation";
 
 const Footer = () => {
-    return(
-        <div>
-            <div className='footer-wrap'>
-                <div className='footer-nav row'>
-                    <div className='footer-logo-brand col-md-6'>
-                        <p className='footer-logo-label'><FaVine className='footer-logo' />COVID19G0</p>
-                    </div>
-                    <ul className="col-md-6">
-                        <i><FaTwitter /></i>
-                        <i><FaFacebook /></i>
-                        <i><FaInstagram /></i>
-                        <i><FaYoutube /></i>
-                        <i><FaWifi /></i>
-                    </ul>
-                </div>
-
-                <div className='footer-item'>
-                    <span>
-                        <li> Channels</li>
-                        <li> News Letters</li>
-                        <li> Apps</li>
-                        <li> Directory</li>
-                        <li> Apps</li>
-                    </span>
-                    <span>
-                        <li> Directory</li>
-                        <li> Channels</li>
-                        <li> Directory</li>
-                        <li> Channels</li>
-                        <li> Apps</li>
-                    </span>
-                    <span>
-                        <li> Podcasts</li>
-                        <li> News Letters</li>
-                        <li> Directory</li>
-                        <li> Channels</li>
-                        <li> Apps</li>
-                    </span>
-                </div>
-            </div>
+  return (
+    <div className="footer-wrap">
+      <div className="footer-nav row">
+        <div className="footer-logo-brand col-md-6">
+          <p className="footer-logo-label">
+            <FaVine className="footer-logo" />
+            COVID19G0
+          </p>
         </div>
-    )
-}
+        <ul className="col-md-6">
+          <i>
+            <FaTwitter />
+          </i>
+          <i>
+            <FaFacebook />
+          </i>
+          <i>
+            <FaInstagram />
+          </i>
+          <i>
+            <FaYoutube />
+          </i>
+          <i>
+            <FaWifi />
+          </i>
+        </ul>
+      </div>
+
+      <div className="footer-item">
+        <span>
+          <li> Channels</li>
+          <li> News Letters</li>
+          <li> Apps</li>
+          <li> Directory</li>
+          <li> Apps</li>
+        </span>
+        <span>
+          <li> Directory</li>
+          <li> Channels</li>
+          <li> Directory</li>
+          <li> Channels</li>
+          <li> Apps</li>
+        </span>
+        <span>
+          <li> Podcasts</li>
+          <li> News Letters</li>
+          <li> Directory</li>
+          <li> Channels</li>
+          <li> Apps</li>
+        </span>
+      </div>
+    </div>
+  );
+};
 export default Footer;
--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,30 @@
-import  React, { Fragment } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navigation from './components/Navigation/Navigation';
-import Home from './pages/Home/Home';
-import Donate from './pages/Donate/Donate';
-import News from './pages/News/News';
-import Footer from './components/Footer/Footer';
-import Login from './pages/Login/Login';
+import React, { Fragment } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import Home from "./pages/Home/Home";
+import Donate from "./pages/Donate/Donate";
+import News from "./pages/News/News";
+import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login/Login";
 import Success from "./components/Success/Success";
 
 const Routes = () => {
-    return(
-        <Fragment>
-            <BrowserRouter>
-            <Navigation />
-                <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route exact path='/home' component={Home} />
-                    <Route exact path='/donate' component={Donate} />
-                    <Route exact path='/news' component={News} />
-                    <Route exact path='/login' component={Login} />
-                    <Route exact path='/success' component={Success} />
-                    {/* <Route exact path="/Donate/:checkoutId" component={Checkout} /> */}
-                </Switch>
-        </BrowserRouter>
-        <Footer />
+  return (
+    <Fragment>
+      <BrowserRouter>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/donate" component={Donate} />
+          <Route exact path="/news" component={News} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/success" component={Success} />
+          {/* <Route exact path="/Donate/:checkoutId" component={Checkout} /> */}
+        </Switch>
+      </BrowserRouter>
+      {/* <Footer /> */}
     </Fragment>
-    )
-   
-} 
+  );
+};
 export default Routes;
