9c675d23f773dbc5ec6802149017b26e41283cf1
KelahKelah <kelechiekebor@gmail.com>
2020-08-14 13:35:28 +0100

    Fix mobile responsiveness

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,39 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { FaVine, FaCaretDown, FaUserAlt, FaAlignJustify } from 'react-icons/fa';
-import './Navigation.css';
+import React, { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FaVine,FaTimes, FaUserAlt, FaAlignJustify } from "react-icons/fa";
+import styles from "./Navigation.module.css";
 
 const Navigation = () => {
-    // const [toggle, setToggle] = useState()
-    // useEffect() {
+  const [toggle, setToggle] = useState(false);
+  // useEffect() {
+  // }
+  const OpenNav = () => {
+    // const myLoop = document.querySelectorAll(".item");
+    // var i;
+    // for (i = 0; i < myLoop.length; i++) {
+    //   console.log(myLoop[i]);
+    //   myLoop[i].style.display = "block";
     // }
-    const OpenNav = () => {
-        const myLoop = document.querySelectorAll('.item')
-        var i;
-         for(i=0; i < myLoop.length; i++) {
-        console.log(myLoop[i])
-           myLoop[i].style.display = 'block'
-        }
-    }
-    return(        
-         <>
-            <div className='nav-wrap'>
-                <div className='logo-brand'>
-                    <p className='logo-label'><FaVine className='logo' />COVID19G0</p>
-                </div>
-                <div className='wrap'>
-                    <p><Link to='/' className='item'>Home</Link></p>
-                    <p><NavLink to='/news' className='item'>News</NavLink></p>
-                    <p><NavLink to='/donate' className='item'>Donate</NavLink></p>
-                    <p><NavLink to='/contact' className='item'>Contact</NavLink></p>
-                    {/* <p className='icon'><FaAlignJustify onClick={OpenNav} /></p> */}
-                </div>
-                <div className="admin">
-                    <p><NavLink to='/login' className='item'><FaUserAlt /> </NavLink></p>
-                </div>
-            </div>
-        </>
-    )
-
-}
+  };
+  return toggle ? (
+    <div>
+      <div className={styles.sideNav}>
+        <p> <FaTimes /></p>
+        <p>
+          <Link to="/" className={styles.sideNavItem}>
+            Home
+          </Link>
+        </p>
+        <p>
+          <NavLink to="/news" className={styles.sideNavItem}>
+            News
+          </NavLink>
+        </p>
+        <p>
+          <NavLink to="/donate" className={styles.sideNavItem}>
+            Donate
+          </NavLink>
+        </p>
+        <p>
+          <NavLink to="/contact" className={styles.sideNavItem}>
+            Contact
+          </NavLink>
+        </p>
+      </div>
+    </div>
+  ) : (
+    <div className={styles.navContainer}>
+      <div className={styles.logoBrand}>
+        <p className={styles.logoLabel}>
+          <FaVine className={styles.logo} />
+          COVID19G0
+        </p>
+      </div>
+      <div className={styles.wrap}>
+        <p>
+          <Link to="/" className={styles.item}>
+            Home
+          </Link>
+        </p>
+        <p>
+          <NavLink to="/news" className={styles.item}>
+            News
+          </NavLink>
+        </p>
+        <p>
+          <NavLink to="/donate" className={styles.item}>
+            Donate
+          </NavLink>
+        </p>
+        <p>
+          <NavLink to="/contact" className={styles.item}>
+            Contact
+          </NavLink>
+        </p>
+      </div>
+      <div className={styles.admin}>
+        <p>
+          <NavLink to="/login" className={styles.item}>
+            <FaUserAlt />{" "}
+          </NavLink>
+        </p>
+      </div>
+      <p className={styles.menu} onClick={()=>setToggle(true)}>
+        <FaAlignJustify />
+      </p>
+    </div>
+  );
+};
 export default Navigation;
--- src/pages/Donate/Donate.js
+++ src/pages/Donate/Donate.js
@@ -1,133 +1,209 @@
-import React, { useEffect, useState } from 'react';
-import {Link} from 'react-router-dom';
-import './Donate.css';
-import {FaQuoteLeft, FaQuoteRight } from 'react-icons/fa'
-import Success from '../../components/Success/Success';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Donate.module.css";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+import Success from "../../components/Success/Success";
 
 const Donate = (props) => {
-    const [inputs, setInputs] = useState({fullName:'', email: '', paymentMethod: '',
-    cardNumber: '', addCvv: '', expiryDate: '',amount: ''});
-    const [data, setDate] = useState({fullName: ''});
-    const [success, setSucess] = useState(false);
-    // console.log('CHECKING TEST',props.location.state)
-    const { state } = props.location
-    // useEffect(() => {
-    //     const {submit} = props
-    //     console.log("checking",submit)
+  const [inputs, setInputs] = useState({
+    fullName: "",
+    email: "",
+    paymentMethod: "",
+    cardNumber: "",
+    addCvv: "",
+    expiryDate: "",
+    amount: "",
+  });
+  const [data, setDate] = useState({ fullName: "" });
+  const [success, setSucess] = useState(false);
+  // console.log('CHECKING TEST',props.location.state)
+  const { state } = props.location;
+  // useEffect(() => {
+  //     const {submit} = props
+  //     console.log("checking",submit)
 
-    // console.log("checking",state)
-   
-    const handleInputs = (e) => {
-        console.log('checking for events',e)
-        setInputs({...inputs, [e.target.name]: e.target.value})
+  // console.log("checking",state)
 
-        console.log('Inputs after setting input',inputs)
+  const handleInputs = (e) => {
+    console.log("checking for events", e);
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+
+    console.log("Inputs after setting input", inputs);
+  };
+
+  const handleModalSubmit = (input) => {
+    props.history.push({
+      pathname: "/Success",
+      // pathname: `/Success${input}`,
+      state: "input.name",
+    });
+    if (
+      inputs.fullName &&
+      inputs.email &&
+      inputs.paymentMethod &&
+      inputs.cardNumber &&
+      inputs.cardNumber &&
+      inputs.addCvv &&
+      inputs.expiryDate &&
+      inputs.amount
+    ) {
     }
-
-    const handleModalSubmit = (input) => {
-        props.history.push(
-            {
-                pathname: '/Success',
-                // pathname: `/Success${input}`,
-                state: 'input.name'
-            }
-        )
-            if(inputs.fullName && inputs.email && inputs.paymentMethod && inputs.cardNumber && 
-                inputs.cardNumber && inputs.addCvv && inputs.expiryDate && inputs.amount) {
-                    
-                }
-    }
-    return (
-        <div className="donate-wrap">
-        
-            <div className='heading'>
-                <h2>Donate to Covid19GO!</h2>
-            </div>
-            <div className='wrap-two'>
-                <div className="donate-text">
-                    
-                    <p><i><FaQuoteLeft /></i> Covid19GO is an independent, non-profit organization passionate about helping covid19 victims all over the world. 
-                            This includes sponsoring victims for proper medical care, providing essentials such as drugs, food items, 
-                            facemask and so much more. 
-                    </p>
-                    <p>We believe that our contributions will go a long way in kicking covid19 out of our universe. We rely entirely 
-                        on donations and receive no money from any governments or corporations. Our tiny online team ensures even the 
-                        smallest contributions go a long way.
-                    </p>
-                    <p>If you have any questions about how we use funds, 
-                        please feel free to email us <Link to="">here</Link>. Our payment process is fully encrypted to 
-                        ensure all information is safe with us  <i><FaQuoteRight  /></i>
-                    </p> 
-                    
-                </div>
-                
-                <div className="payment">
-                    <button type="button" data-toggle="modal" data-target="#paymentModal" className="btn btn-warning" >Donate</button>
-                </div>           
-                        
-            </div>
-
-        
-
-            {/* Modal                */}
-            {success? 
-            <Success fullName={inputs.fullName} /> :
-        
-            <div className="modal fade" id="paymentModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h3 className="modal-title" id="exampleModalLabel">Add payment info</h3>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <form onSubmit={handleModalSubmit} >
-                                <div className="form-group">
-                                    <label>Full name</label>
-                                    <input type="text" name="fullName" value={inputs.fullName} onChange={handleInputs} required />
-                                </div>
-                                <div className="form-group">
-                                    <label>Email </label>
-                                    <input type="email" name="email" value={inputs.email} onChange={handleInputs} required />
-                                </div>
-                                <div className="form-group">
-                                    <label>Payment Method</label>
-                                    <input type="select" name="paymentMethod" value={inputs.paymentMethod} onChange={handleInputs} required />
-                                </div>
-                                <div className="form-group">
-                                    <label>Card number</label>
-                                    <input type="number" name="cardNumber" value={inputs.cardNumber} onChange={handleInputs} required />
-                                </div>
-                                <div className="form-group">
-                                    <label>Add Cvv</label>
-                                    <input type="number" name="addCvv" value={inputs.addCvv} onChange={handleInputs} required />
-                                </div>
-                                <div className="form-group">
-                                    <label>Expiry date</label>
-                                    <input type="number" name="expiryDate" value={inputs.expiryDate} onChange={handleInputs} required />
-                                </div>
-                                <div className="form-group">
-                                    <label>Amount</label>
-                                    <input type="number" name="amount" value={inputs.amount} onChange={handleInputs} required />
-                                </div>
-                                <div className="modal-footer">
-                                    <button type="submit" className="btn btn-warning">Pay</button>
-                                </div>
-                            </form>
-                            
-                        </div>
-                        
-                    </div>
-                </div>
-            </div> }
-
+  };
+  return (
+    <div className={styles.donateWrap}>
+      <div className={styles.heading}>
+        <h2>Donate to Covid19GO!</h2>
+      </div>
+      <div className={styles.wrapTwo}>
+        <div className={styles.donateText}>
+          <p>
+            <i>
+              <FaQuoteLeft />
+            </i>{" "}
+            Covid19GO is an independent, non-profit organization passionate
+            about helping covid19 victims all over the world. This includes
+            sponsoring victims for proper medical care, providing essentials
+            such as drugs, food items, facemask and so much more.
+          </p>
+          <p>
+            We believe that our contributions will go a long way in kicking
+            covid19 out of our universe. We rely entirely on donations and
+            receive no money from any governments or corporations. Our tiny
+            online team ensures even the smallest contributions go a long way.
+          </p>
+          <p>
+            If you have any questions about how we use funds, please feel free
+            to email us <Link to="">here</Link>. Our payment process is fully
+            encrypted to ensure all information is safe with us{" "}
+            <i>
+              <FaQuoteRight />
+            </i>
+          </p>
         </div>
-            
-    )
-}
+
+        <div className={styles.payment}>
+          <button
+            type="button"
+            data-toggle="modal"
+            data-target="#paymentModal"
+            className="btn btn-warning"
+          >
+            Donate
+          </button>
+        </div>
+      </div>
+
+      {/* Modal                */}
+      {success ? (
+        <Success fullName={inputs.fullName} />
+      ) : (
+        <div
+          className="modal fade"
+          id="paymentModal"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h3 className="modal-title" id="exampleModalLabel">
+                  Add payment info
+                </h3>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <form onSubmit={handleModalSubmit}>
+                  <div className="form-group">
+                    <label>Full name</label>
+                    <input
+                      type="text"
+                      name="fullName"
+                      value={inputs.fullName}
+                      onChange={handleInputs}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Email </label>
+                    <input
+                      type="email"
+                      name="email"
+                      value={inputs.email}
+                      onChange={handleInputs}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Payment Method</label>
+                    <input
+                      type="select"
+                      name="paymentMethod"
+                      value={inputs.paymentMethod}
+                      onChange={handleInputs}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Card number</label>
+                    <input
+                      type="number"
+                      name="cardNumber"
+                      value={inputs.cardNumber}
+                      onChange={handleInputs}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Add Cvv</label>
+                    <input
+                      type="number"
+                      name="addCvv"
+                      value={inputs.addCvv}
+                      onChange={handleInputs}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Expiry date</label>
+                    <input
+                      type="number"
+                      name="expiryDate"
+                      value={inputs.expiryDate}
+                      onChange={handleInputs}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Amount</label>
+                    <input
+                      type="number"
+                      name="amount"
+                      value={inputs.amount}
+                      onChange={handleInputs}
+                      required
+                    />
+                  </div>
+                  <div className="modal-footer">
+                    <button type="submit" className="btn btn-warning">
+                      Pay
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 export default Donate;
-
-
-
