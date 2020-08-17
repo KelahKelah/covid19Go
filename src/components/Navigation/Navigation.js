0b89db5d34acdeffb235783ed575dc782b53eda0
KelahKelah <kelechiekebor@gmail.com>
2020-08-17 13:01:01 +0100

    set sidebar menu onclick

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,42 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import { Link, NavLink, withRouter } from 'react-router-dom';
-import { FaVine, FaCaretDown, FaUserAlt, FaAlignJustify } from 'react-icons/fa';
-import  styles  from './Navigation.module.css';
+import React, { useState, useEffect } from "react";
+import { Link, NavLink, withRouter } from "react-router-dom";
+import { FaVine, FaCaretDown, FaUserAlt, FaAlignJustify } from "react-icons/fa";
+import styles from "./Navigation.module.css";
 
 const Navigation = (props) => {
-    // const [toggle, setToggle] = useState()
-    // useEffect() {
-    // }
-    const OpenNav = () => {
-        const myLoop = document.querySelectorAll('.item')
-        var i;
-         for(i=0; i < myLoop.length; i++) {
-        console.log(myLoop[i])
-           myLoop[i].style.display = 'block'
-        }
+  const [toggle, setToggle] = useState(false);
+  // useEffect() {
+  // }
+  const OpenNav = () => {
+    const myLoop = document.querySelectorAll(".item");
+    var i;
+    for (i = 0; i < myLoop.length; i++) {
+      console.log(myLoop[i]);
+      myLoop[i].style.display = "block";
     }
-    return(        
-         <>
-            <div className={styles.navContainer}>
-                <div className={styles.logoBrand}>
-                    <p className={styles.logoLabel}><FaVine className={styles.logo} />COVID19G0</p>
-                </div>
-                <div className={styles.wrap}>
-                    <p><Link to='/' className={styles.item}>Home</Link></p>
-                    <p><NavLink to='/news' className={styles.item}>News</NavLink></p>
-                    <p><NavLink to='/donate' className={styles.item}>Donate</NavLink></p>
-                    <p><NavLink to='/contact' className={styles.item}>Contact</NavLink></p>
-                    {/* <p className='icon'><FaAlignJustify onClick={OpenNav} /></p> */}
-                </div>
-                <div className={styles.admin}>
-                    <p><NavLink to='/login' className={styles.item}><FaUserAlt /> </NavLink></p>
-                </div>    
-                <div className={styles.menu}>
-                    <p className={styles.item}><FaAlignJustify /> </p>
-                </div>   
-            </div>
-        </>
-    )
-}
+  };
+  return toggle ? (
+    <div className={styles.sideNav}>
+
+    </div>
+  ) : (
+    <div className={styles.navContainer}>
+      <div className={styles.logoBrand}>
+        <p className={styles.logoLabel}>
+          <FaVine className={styles.logo} />
+          COVID19G0
+        </p>
+      </div>
+      <div className={styles.wrap}>
+        <p>
+          <Link to="/" className={styles.item}>
+            Home
+          </Link>
+        </p>
+        <p>
+          <NavLink to="/news" className={styles.item}>
+            News
+          </NavLink>
+        </p>
+        <p>
+          <NavLink to="/donate" className={styles.item}>
+            Donate
+          </NavLink>
+        </p>
+        <p>
+          <NavLink to="/contact" className={styles.item}>
+            Contact
+          </NavLink>
+        </p>
+        {/* <p className='icon'><FaAlignJustify onClick={OpenNav} /></p> */}
+      </div>
+      <div className={styles.admin}>
+        <p>
+          <NavLink to="/login" className={styles.item}>
+            <FaUserAlt />{" "}
+          </NavLink>
+        </p>
+      </div>
+      <div className={styles.menu}>
+        <p
+          className={styles.item}
+          onClick={() => {
+            setToggle(true);
+          }}
+        >
+          <FaAlignJustify />{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
 // withRouter for props.history.push of logout
 export default withRouter(Navigation);
